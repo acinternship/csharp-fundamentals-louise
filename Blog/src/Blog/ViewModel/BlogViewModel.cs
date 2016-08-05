7aeda12267c14def799e52a5934e3dd5533a3704
Louise Bougleux <lbougleux@avenuecode.com>
2016-08-05 16:01:30 -0300

    Commit 4

--- Blog/src/Blog/ViewModel/BlogViewModel.cs
+++ Blog/src/Blog/ViewModel/BlogViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,8 @@ namespace Blog.ViewModel
 {
     public class BlogViewModel
     {
+        [Required, MaxLength(80)]
+        public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
     }
 }
